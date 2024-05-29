0491bd2cfe4658ae4eee7816869eed612a97be6d
Anjali Kushwaha <69588336+anjali-kushwaha@users.noreply.github.com>
2024-05-29 10:55:27 +0530

    Time: 49 ms (70.45%), Space: 48.8 MB (44.19%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+function fn(n){
+    return n+1;
+}
+
+var map = function(arr, fn) {
+   return arr.map(fn);
+};
